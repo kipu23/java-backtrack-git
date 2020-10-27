0dbb95d02f6cf2efcd97be7b05834b62f0e93751
Krisztian Szep <krisztian.szep@dxc.com>
2020-10-27 12:39:25 +0100

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+`
+0src/main/java/com/krisz/backtrack/Backtrack.java,0\9\0968645bd6fb1a72a96351c2d1a510d3f6f9f2f4
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/com/krisz/backtrack/Backtrack.java
@@ -0,0 +1,32 @@
+package com.krisz.backtrack;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+public class Backtrack {
+
+    private static final Logger logger = LoggerFactory.getLogger(Backtrack.class);
+
+    public static void main(String[] args) {
+
+        try {
+            Enumeration<URL> resources = Backtrack.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                Manifest manifest = new Manifest(resources.nextElement().openStream());
+                Attributes attr = manifest.getMainAttributes();
+                String shaNumber = attr.getValue("Git-SHA");
+                logger.info("Git SHA number: {}", shaNumber);
+            }
+        } catch (IOException E) {
+            logger.error("Manifest file not found!");
+        }
+
+    }
+
+}
